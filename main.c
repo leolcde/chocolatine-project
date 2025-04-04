c3104d1d282f727e264535b09c90817f6f802bc5
Leo Lacordaire <leo.lacordaire@epitech.eu>
2025-04-04 15:29:18 +0200

    a

--- main.c
+++ main.c
@@ -7,9 +7,11 @@
 
 #include <stdio.h>
 
-int main() {
-	int i = 0;
-	for (; i <= 3; i++)
-		printf("%d ", i);
-	printf("\n");
+int main(void)
+{
+    int i = 0;
+
+    for (; i <= 3; i++)
+        printf("%d ", i);
+    printf("\n");
 }
